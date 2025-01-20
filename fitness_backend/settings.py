428a0eadde781a19c20682d619662d846601c32c
Deepak Bhagat <deepak988088@gmail.com>
2025-01-20 21:27:00 +0530

    feat: changed to local postgres

--- fitness_backend/settings.py
+++ fitness_backend/settings.py
@@ -81,27 +81,27 @@ WSGI_APPLICATION = "fitness_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'fitness_db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'root',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),        # Name of your database (from Render config)
-        'USER': os.getenv('DB_USER'),        # Username for the database (from Render config)
-        'PASSWORD': os.getenv('DB_PASSWORD'),# Password for the database (from Render config)
-        'HOST': os.getenv('DB_HOST'),        # Host URL for the Render DB
-        'PORT': os.getenv('DB_PORT'),        # Port (usually 5432 for PostgreSQL)
+        'NAME': 'fitness_db',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),        # Name of your database (from Render config)
+#         'USER': os.getenv('DB_USER'),        # Username for the database (from Render config)
+#         'PASSWORD': os.getenv('DB_PASSWORD'),# Password for the database (from Render config)
+#         'HOST': os.getenv('DB_HOST'),        # Host URL for the Render DB
+#         'PORT': os.getenv('DB_PORT'),        # Port (usually 5432 for PostgreSQL)
+#     }
+# }
 
 # DATABASES = {
 #     'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
