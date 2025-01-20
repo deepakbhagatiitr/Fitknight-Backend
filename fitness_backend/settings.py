5e98a73e9cb682cb82ec2880d7aa71b9080a3c0b
Deepak Bhagat <deepak988088@gmail.com>
2025-01-20 19:46:07 +0530

    feat: adding env

--- fitness_backend/settings.py
+++ fitness_backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-n%hi$dzm-^r^riq0wa(0u-y3@cil^#v+l52mlfzs$vz5*m6_!i"
+SECRET_KEY = os.getenv('SECRET_KEY', 'development_fallback_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
